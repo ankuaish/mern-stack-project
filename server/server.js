839a953d6fe21df79ea45de202d87706cf8b90fb
ankuaish <anku.kesharwani17@gmail.com>
2024-06-23 23:43:28 +0530

    express

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.status(200).send("Welcome to the world best mern series");
+});
+
+app.get("/register", (req, res) => {
+  res.status(200).send("Welcome to the registration Page");
+});
+
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`server is running at port : ${PORT}`);
+});
