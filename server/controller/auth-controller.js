d4668a42512199fc18ceedc6dee711c1fe52b5f0
ankuaish <anku.kesharwani17@gmail.com>
2024-06-25 03:41:10 +0530

    server

--- server/controller/auth-controller.js
+++ server/controller/auth-controller.js
@@ -10,9 +10,10 @@ const home = async (req, res) => {
 
 const register = async (req, res) => {
   try {
-    res.status(200).send("Welcome to the registration Page using controller");
+    console.log(req.body);
+    res.status(200).json({ message: req.body });
   } catch (error) {
-    res.status(400).send({ msg: "page not found" });
+    res.status(500).json({ msg: "Internal Server Error" });
   }
 };
 
--- server/router/auth-router.js
+++ server/router/auth-router.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const authControllers = require("../controllers/auth-controller");
+const authControllers = require("../controller/auth-controller");
 
 // router.get("/", (req, res) => {
 //   res.status(200).send("Welcome to the world best mern series using router");
@@ -8,6 +8,6 @@ const authControllers = require("../controllers/auth-controller");
 
 router.route("/").get(authControllers.home);
 
-router.route("/register").get(authControllers.register);
+router.route("/register").post(authControllers.register);
 
 module.exports = router;
