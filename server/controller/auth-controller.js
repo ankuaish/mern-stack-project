28eed38553593d697f5949c04c458405b6bbc057
ankuaish <anku.kesharwani17@gmail.com>
2024-07-26 21:55:59 +0530

    register

--- server/controller/auth-controller.js
+++ server/controller/auth-controller.js
@@ -1,3 +1,5 @@
+const User = require("../models/user-model");
+
 const home = async (req, res) => {
   try {
     res
@@ -11,9 +13,17 @@ const home = async (req, res) => {
 const register = async (req, res) => {
   try {
     console.log(req.body);
-    res.status(200).json({ message: req.body });
+    const { username, email, phone, password } = req.body;
+
+    const userExist = await User.findOne({ email });
+
+    if (userExist) {
+      return res.status(400).json({ msg: "email already exists" });
+    }
+    const userCreated = await User.create({ username, email, phone, password });
+    res.status(201).json({ msg: userCreated });
   } catch (error) {
-    res.status(500).json({ msg: "Internal Server Error" });
+    res.status(500).json({ message: "Internal Server Error" });
   }
 };
 
