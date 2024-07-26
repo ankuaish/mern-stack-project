b8e88d276ded48dc5de3fb0e04a1723f35a9b5ac
ankuaish <anku.kesharwani17@gmail.com>
2024-07-26 23:26:02 +0530

    bcrypt

--- server/controller/auth-controller.js
+++ server/controller/auth-controller.js
@@ -1,4 +1,5 @@
 const User = require("../models/user-model");
+const bcrypt = require("bcrypt.js");
 
 const home = async (req, res) => {
   try {
@@ -20,7 +21,14 @@ const register = async (req, res) => {
     if (userExist) {
       return res.status(400).json({ msg: "email already exists" });
     }
-    const userCreated = await User.create({ username, email, phone, password });
+    const saltRound = 10;
+    const hash_password = await bcrypt.hash(password, saltRound);
+    const userCreated = await User.create({
+      username,
+      email,
+      phone,
+      password: hash_password,
+    });
     res.status(201).json({ msg: userCreated });
   } catch (error) {
     res.status(500).json({ message: "Internal Server Error" });
